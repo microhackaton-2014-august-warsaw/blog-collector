23381a6bcffbdf3c9d286f8bfef9ed290fb63f26
Tomasz Dziurko <tdziurko@gmail.com>
2014-08-09 19:04:27 +0200

    Invalid urls are now handled properly even if Jarek sais otherwise

--- src/main/groovy/com/ofg/microservice/blog/rss_fetching/RssFetcher.groovy
+++ src/main/groovy/com/ofg/microservice/blog/rss_fetching/RssFetcher.groovy
@@ -4,6 +4,7 @@ import akka.actor.UntypedActor
 import com.google.common.base.Stopwatch
 import com.ofg.infrastructure.web.filter.correlationid.CorrelationIdHolder
 import com.ofg.microservice.blog.rss_fetching.data_extraction.RssData
+import com.rometools.fetcher.FetcherException
 import com.rometools.fetcher.impl.HttpURLFeedFetcher
 import com.rometools.rome.feed.synd.SyndFeed
 import groovy.transform.TypeChecked
@@ -28,15 +29,18 @@ public class RssFetcher extends UntypedActor {
     void processRequest(RssFetchRequest fetchRequest) throws Exception {
         MDC.put(CorrelationIdHolder.CORRELATION_ID_HEADER, fetchRequest.correlationId)
 
-        Stopwatch stopwatch = Stopwatch.createStarted();
-        log.info("Fetching rss from " + fetchRequest.rssUrl)
-        HttpURLFeedFetcher fetcher = new HttpURLFeedFetcher()
-        SyndFeed feed = fetcher.retrieveFeed(new URL(fetchRequest.rssUrl))
-        long executionTime = stopwatch.stop().elapsed(TimeUnit.MILLISECONDS)
-        log.info("Fetched " + feed.entries.size() + " after " + executionTime + " ms")
-        this.context.system().eventStream()
-                .publish(new RssData(fetchRequest.rssUrl, feed, fetchRequest.pairId, fetchRequest.correlationId))
+        try {
+            Stopwatch stopwatch = Stopwatch.createStarted();
+            log.info("Trying to fetch rss from " + fetchRequest.rssUrl)
+            HttpURLFeedFetcher fetcher = new HttpURLFeedFetcher()
+            SyndFeed feed = fetcher.retrieveFeed(new URL(fetchRequest.rssUrl))
+            long executionTime = stopwatch.stop().elapsed(TimeUnit.MILLISECONDS)
+            log.info("Fetched " + feed.entries.size() + " after " + executionTime + " ms")
+            this.context.system().eventStream()
+                    .publish(new RssData(fetchRequest.rssUrl, feed, fetchRequest.pairId, fetchRequest.correlationId))
+        } catch (MalformedURLException | FetcherException e) {
+            log.error("Non existing rss url " + fetchRequest.rssUrl)
+        }
     }
-
 
 }
